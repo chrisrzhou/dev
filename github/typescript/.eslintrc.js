c9b11a5cae4d698d9e75c1be9c4c9ba4f159c372
Chris Zhou <chrisrzhou@pm.me>
2019-08-08 18:09:46 -0700

    remove docz and update

--- github/typescript/.eslintrc.js
+++ github/typescript/.eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
     'react-hooks',
   ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
